03d3721858d982ed6cac9883e436c942639c989f
Markuss Daniels Lankovskis <129846561+Markusinsh@users.noreply.github.com>
2023-11-16 14:57:17 +0200

    1.Uzdevums.py

--- 1.Uzdevums.py
+++ 1.Uzdevums.py
@@ -1,1 +1,10 @@
+#lai lietotajs ieraksta skaitli
+ievaditais_skaitlis = int(input("Ievadiet skailti:"))
 
+#Cikls kad skaitaw
+summa = 0
+for skaitlis in range(1, ievaditais_skaitlis + 1):
+    summa += skaitlis
+
+#parada gala rezultatu
+print(f"Skaitļu no 1 līdz{ievaditais_skaitlis} summa ir {summa}")
