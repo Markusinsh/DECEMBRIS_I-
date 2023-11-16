2b8c5f60d9c537c7e784e3a824ecd3a6d607356f
Markuss Daniels Lankovskis <129846561+Markusinsh@users.noreply.github.com>
2023-11-16 15:02:54 +0200

    Create 2.Uzdevums.py

---
+++ 2.Uzdevums.py
@@ -0,0 +1,8 @@
+skaitlis = 1
+
+# Izmantoju while ciklu, lai atrastu pirmo skaitli, kuram kvadraats ir lielāks par 1000
+while skaitlis**2 <= 1000:
+    skaitlis += 1
+
+# Parada terminali dabūto skaitli
+print("Pirmais skaitlis, kura kvadrāts ir lielāks par 1000, ir:", skaitlis)
