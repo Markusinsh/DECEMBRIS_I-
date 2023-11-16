741dedc721bbe0ded920873d879c8e19c285ed2a
Markuss Daniels Lankovskis <129846561+Markusinsh@users.noreply.github.com>
2023-11-16 15:07:57 +0200

    Jauns

---
+++ 3.Uzdevums.py
@@ -0,0 +1,10 @@
+# vai skaitlis ir nepāra
+
+# Ievadīju skaitli 12 testam
+ievaditais_skaitlis = int("12")
+
+# Parāda vai skaitlis ir vai nav pāra
+if ievaditais_skaitlis % 2 != 0:
+    print(f"Skaitlis {ievaditais_skaitlis} ir nepāra.")
+else:
+    print(f"Skaitlis {ievaditais_skaitlis} ir pāra.")
